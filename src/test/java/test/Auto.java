eb823ee66a2ab6c84ee9af120a689a467e04b8d2
DavidToroArboleda <142697635+DavidToroArboleda@users.noreply.github.com>
2023-08-21 20:59:39 -0500

    clase Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,5 +1,47 @@
 package test;
 
-public class Auto {
 
+
+public class Auto {
+	String modelo;
+	int precio;
+	String marca;
+	int registro;
+	Motor motor;
+	Asiento[] asientos;
+	
+	
+	static int cantidadCreados;
+
+public int cantidadAsientos() {
+		
+		int NumeroAsientos = 0;
+		for (int i = 0; i < asientos.length; i++) {
+			if (asientos[i] != null) {
+				NumeroAsientos++;
+			}
+		}
+		return NumeroAsientos;
+					
+	}
+	
+	public String verificarIntegridad() {
+		boolean verificarAsientos = true;
+		for (int i = 0; i < asientos.length; i++) {
+			if (asientos[i] != null) {
+				if (registro != asientos[i].registro) {
+					verificarAsientos = false;
+					break;
+				}
+	
+				}
+			}
+		if (registro == motor.registro && verificarAsientos == true) {
+			return "Auto original";
+		}else {
+			return "Las piezas no son originales";
+		}
+	
+	
+	}
 }
